cb8aaa17e680e830e91a46d695fc3ff52d0d7525
yoon <58201514+onlyoon@users.noreply.github.com>
2023-10-04 18:58:13 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("");
+
+
+function solution(arr) {
+  frequencyCounter = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0
+  };
+  for (let val of arr) {
+    // console.log(val);
+    frequencyCounter[val] += 1;
+  }
+  // console.log(frequencyCounter);
+  result = [];
+  for (val in frequencyCounter) {
+    result.push(frequencyCounter[val]);
+  }
+
+  return result.join(" ");
+}
+
+console.log(solution(input));
